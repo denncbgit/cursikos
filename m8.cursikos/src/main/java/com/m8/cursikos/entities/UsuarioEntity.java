ed930492288c4ce539a43d81f8293e89afa16842
Nerffren <nerffren@nerffren.nerffren>
2020-04-24 13:56:38 +0200

    First commit iniciacion de proyecto con Spring boot

---
+++ m8.cursikos/src/main/java/com/m8/cursikos/entities/UsuarioEntity.java
@@ -0,0 +1,127 @@
+package com.m8.cursikos.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+public class UsuarioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	
+	@Pattern(regexp = "^[A-Za-z0-9_-]{2,16}$", message ="El nombre de Usuario debe tener como minimo 2 letras y como maximo 16 y solo debe contener (mayusculas, minusculas, numeros, '-' y '_')")
+	@NotEmpty
+	private String username;
+	
+	
+	@Email
+	@Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", message = "La sintaxis del email es incorrecta")
+	@NotEmpty
+	private String email;
+	
+	@Pattern(regexp = "(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}$", message = "Recuerde que debe tener 8 caracteres y almenos, un digito, una letra mayus y minus y un simbolo" )
+	@NotEmpty
+	private String password;
+	
+	
+	public UsuarioEntity() {}
+	
+	
+	
+	
+	public UsuarioEntity(String username,String email,String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioEntity other = (UsuarioEntity) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UsuarioEntity [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+ "]";
+	}
+	
+	
+	
+	
+}
