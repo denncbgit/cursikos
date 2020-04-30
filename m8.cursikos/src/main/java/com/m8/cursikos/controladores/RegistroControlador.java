db2e5dc97a0962cc2880c4c10f275db74e29d8ea
Nerffren <nerffren@nerffren.nerffren>
2020-04-30 22:18:09 +0200

    remove coment login

--- m8.cursikos/src/main/java/com/m8/cursikos/controladores/RegistroControlador.java
+++ m8.cursikos/src/main/java/com/m8/cursikos/controladores/RegistroControlador.java
@@ -85,10 +85,6 @@ public class RegistroControlador {
 	}
 	
 	
-//	@GetMapping("/formLogin")
-//	public String form(Model model , Usuario usuario ) {
-//		model.addAttribute("usuario", new Usuario());
-//		return "formLogin";
-//	}
+
 
 }
