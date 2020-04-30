661f477eda127ef22739b20ebd3bd3b6b75fc618
Nerffren <nerffren@nerffren.nerffren>
2020-04-30 08:28:40 +0200

    Añadidos test Unitarios

---
+++ m8.cursikos/src/test/java/com/m8/cursikos/testControladores/testComentariosController.java
@@ -0,0 +1,40 @@
+package com.m8.cursikos.testControladores;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.m8.cursikos.controladores.ComentariosController;
+
+
+/**
+ * Comprobar el estado de los controladores de comentarios
+ * @author Nerffren
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class testComentariosController {
+
+
+	 @Autowired
+	 private ComentariosController controller;
+	
+	 @Test
+	 public void contextLoads() {
+	  assertThat(controller).isNotNull();
+	 }
+		  
+
+}
+
+
+   
+
---
+++ m8.cursikos/src/test/java/com/m8/cursikos/testEntidades/testUsuarioEntidad.java
@@ -0,0 +1,45 @@
+package com.m8.cursikos.testEntidades;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.Assert.fail;
+
+import org.hibernate.validator.internal.constraintvalidators.bv.AssertFalseValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.m8.cursikos.entities.Usuario;
+import com.m8.cursikos.repositorios.UsuarioRepositorio;
+
+/**
+ * test sobre la entidad usuario y sus inserciones en la BD de la clase Usuario
+ * @author Nerffren
+ *
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class testUsuarioEntidad {
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	UsuarioRepositorio usuarioRepositorio;
+	
+	@Test
+	 public void busquedaUsuarioByUsername() {
+		String username = "Nerffren";
+		Usuario usuario = new Usuario("Nerffren", "nerffren@gmail.com", "Pjuegko12");
+		usuarioRepositorio.save(usuario);
+	    Usuario found = usuarioRepositorio.findByUsername(username);
+	    
+	    assertThat(found.getUsername())
+	      .isEqualTo(username);
+	 }
+	
+
+}
