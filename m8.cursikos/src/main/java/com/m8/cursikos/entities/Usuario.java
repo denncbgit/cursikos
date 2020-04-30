61ed6b2ac8ac3b41205f62d287465ed90d0d501b
Nerffren <nerffren@nerffren.nerffren>
2020-04-30 21:46:13 +0200

    hashcode usuario

--- m8.cursikos/src/main/java/com/m8/cursikos/entities/Usuario.java
+++ m8.cursikos/src/main/java/com/m8/cursikos/entities/Usuario.java
@@ -133,58 +133,6 @@ public class Usuario {
 
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((EncPassword == null) ? 0 : EncPassword.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (EncPassword == null) {
-			if (other.EncPassword != null)
-				return false;
-		} else if (!EncPassword.equals(other.EncPassword))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (id != other.id)
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
-	}
-
-
-
-
-	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
 				+ ", EncPassword=" + EncPassword + "]";
